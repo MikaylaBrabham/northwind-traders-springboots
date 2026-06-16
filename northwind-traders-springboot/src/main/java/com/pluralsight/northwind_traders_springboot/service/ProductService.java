d3447d48bebf22e779263931a92f0870ead521a5
MikaylaBrabham <MikaylaBrabham01@gmail.com>
2026-06-16 13:23:25 -0500

    copied from his repos from eric273

--- northwind-traders-springboot/src/main/java/com/pluralsight/northwind_traders_springboot/service/ProductService.java
+++ northwind-traders-springboot/src/main/java/com/pluralsight/northwind_traders_springboot/service/ProductService.java
@@ -1,25 +1,30 @@
 package com.pluralsight.northwind_traders_springboot.service;
 
+import com.pluralsight.northwind_traders_springboot.model.Category;
 import com.pluralsight.northwind_traders_springboot.model.Product;
+import com.pluralsight.northwind_traders_springboot.repository.CategoryRepository;
 import com.pluralsight.northwind_traders_springboot.repository.ProductRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ProductService {
 
-    private final ProductRepository productRepository;
+    ProductRepository productRepository;
 
     public ProductService(ProductRepository productRepository) {
         this.productRepository = productRepository;
     }
 
-    public ProductRepository getProductRepository() {
-        return productRepository;
+    public List<Product> getAllProducts(){
+        return productRepository.findAll();
     }
 
-    //add methods
+    public Optional<Product> getProductById(int id) {
+        return productRepository.findById(id);
+    }
 
 
 }
