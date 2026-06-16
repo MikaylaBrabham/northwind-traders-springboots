1f8477d35297317b818e0ad2c2251bb0729050f5
MikaylaBrabham <MikaylaBrabham01@gmail.com>
2026-06-16 13:28:52 -0500

    cited from eric273

--- northwind-traders-springboot/src/main/java/com/pluralsight/northwind_traders_springboot/repository/ProductRepository.java
+++ northwind-traders-springboot/src/main/java/com/pluralsight/northwind_traders_springboot/repository/ProductRepository.java
@@ -7,12 +7,17 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 //added interface for products
 
-        @Repository
-        public interface ProductRepository extends JpaRepository<Product,Integer> {
-            List<Product> findByproductIDIgnoreCase(Integer productID);
-            List<Product> findByNameContainingIgnoreCase(String name);
-            List<Product> findByCategoryIgnoreCase(String category);
-            List<Product> findByPriceContainingIgnoreCase(Double price);
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Integer> {
+}
 
-        }
+
+     //   @Repository
+     //   public interface ProductRepository extends JpaRepository<Product,Integer> {
+        //    List<Product> findByproductIDIgnoreCase(Integer productID);
+      //      List<Product> findByNameContainingIgnoreCase(String name);
+    //        List<Product> findByCategoryIgnoreCase(String category);
+  //          List<Product> findByPriceContainingIgnoreCase(Double price);
+//
+     //   }
 
